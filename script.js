386d6e73a046e5e5d7241c488584109670a65ff8
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-04-25 15:06:26 -0700

    Added recursive conversion for decimal to octal

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ function decimalToBinary(n){
     return decimalToBinary(Math.floor(n/2)) + (n%2);
 }
 
+function decimalToOctal(n){
+    if(n===0) return "0";
+    if(n < 0) return "Invalid input";
+
+    return decimalToOctal(Math.floor(n/8)) + (n % 8);
+}
+
+console.log(decimalToOctal(10))
