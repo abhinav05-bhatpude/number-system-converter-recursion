8cc2a2c40f2c3d681db3352147716d3399f60b41
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-04-25 15:01:01 -0700

    Implemented recursive function for decimal to binary conversion

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Recursion project started");
+
+
+function decimalToBinary(n){
+    if(n===0) return "0";
+    if(n===1) return "1";
+
+    return decimalToBinary(Math.floor(n/2)) + (n%2);
+}
+console.log(decimalToBinary(10))
