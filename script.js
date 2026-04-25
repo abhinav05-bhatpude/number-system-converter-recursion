3326958e5d06250d3802d7137d8e192af5921a46
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-04-25 15:03:23 -0700

    Handled edge cases for decimal to binary conversion

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ console.log("Recursion project started");
 
 function decimalToBinary(n){
     if(n===0) return "0";
-    if(n===1) return "1";
+    if(n < 0) return "Invalid input";
 
     return decimalToBinary(Math.floor(n/2)) + (n%2);
 }
-console.log(decimalToBinary(10))
+
